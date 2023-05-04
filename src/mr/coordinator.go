652d961823fd97ff69d67887aea6acc3f8985a7f
unknowntpo <e850506@gmail.com>
2023-05-04 09:59:56 +0800

    extract rpc call into Mailbox object

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,30 +6,59 @@ import "os"
 import "net/rpc"
 import "net/http"
 
-
 type Coordinator struct {
 	// Your definitions here.
-
+	mailBox MailBox
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
-	rpc.Register(c)
+	c.mailBox.Serve()
+}
+
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+func (c *Coordinator) Done() bool {
+	return c.mailBox.Done()
+}
+
+// create a Coordinator.
+// main/mrcoordinator.go calls this function.
+// nReduce is the number of reduce tasks to use.
+func MakeCoordinator(files []string, nReduce int) *Coordinator {
+	m := &rpcMailBox{}
+	c := Coordinator{mailBox: m}
+
+	// Your code here.
+
+	c.server()
+	return &c
+}
+
+type MailBox interface {
+	Serve()
+	Done() bool
+	Example(args *ExampleArgs, reply *ExampleReply) error
+}
+
+type chanMailBox struct {
+}
+
+type rpcMailBox struct {
+}
+
+func (r *rpcMailBox) Serve() {
+	rpc.Register(r)
 	rpc.HandleHTTP()
 	//l, e := net.Listen("tcp", ":1234")
 	sockname := coordinatorSock()
@@ -41,30 +70,13 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
-// main/mrcoordinator.go calls Done() periodically to find out
-// if the entire job has finished.
-//
-func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+func (r *rpcMailBox) Done() bool {
+	// hang here forever
+	// TODO: Handle this done check
+	return false
 }
 
-//
-// create a Coordinator.
-// main/mrcoordinator.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
-func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-	// Your code here.
-
-
-	c.server()
-	return &c
+func (r *rpcMailBox) Example(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
 }
